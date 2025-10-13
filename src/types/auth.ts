e0aa49f4fc7a60bb0c2e0228d3a92a5c9217e288
wmiltecti <waldemar.ti@miltecti.com.br>
2025-10-13 08:41:43 -0300

    Added auth.ts

---
+++ src/types/auth.ts
@@ -0,0 +1,22 @@
+export type PessoaTipo = "PF" | "PJ" | "PASSAPORTE" | "ESTRANGEIRO";
+
+export interface LoginPF_PJ_Passaporte {
+  tipoDeIdentificacao: "CPF" | "CNPJ" | "PASSAPORTE";
+  numeroIdentificacao: string;
+  senha: string;
+}
+
+export interface LoginEstrangeiro {
+  tipoDeIdentificacao: "ID_ESTRANGEIRA";
+  numeroIdentificacao: string;
+  senha: string;
+}
+
+export type LoginRequest = LoginPF_PJ_Passaporte | LoginEstrangeiro;
+
+export interface LoginResponse {
+  token?: string;
+  userId?: string;
+  nome?: string;
+  perfil?: string;
+}
