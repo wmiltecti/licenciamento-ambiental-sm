7d0995578067fc6f4f1b09693135ecd9353edc6e
wmiltecti <waldemar.ti@miltecti.com.br>
2025-10-14 17:09:07 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'), // <-- habilita import '@/...'
+    },
+  },
   optimizeDeps: {
+    // manter seu exclude
     exclude: ['lucide-react'],
+    // turf modular pré-empacotado p/ dev
     include: ['@turf/buffer', '@turf/helpers', '@turf/difference'],
   },
-});
+})
