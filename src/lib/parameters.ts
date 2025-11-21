00fb5c3ab6e9a670c055097845f065a131c7bbc8
dsakamoto-miltec <deborah.ti@miltecti.com.br>
2025-11-21 16:45:20 -0400

    Updated BlockchainTab.tsx

--- src/lib/parameters.ts
+++ src/lib/parameters.ts
@@ -1,4 +1,5 @@
 export const BLOCKCHAIN_PARAMS = {
   idBlockchain: 5,
-  baseUrl: import.meta.env.VITE_API_BASE_URL
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
+  blockchainExplorerUrl: 'http://continuus.miltecti.com.br/block/'
 };
